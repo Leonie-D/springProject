0ae9502f8e6e467d1a6f06acc7e04a9457145ae0
Audrey Bnt <58702393+audbnt@users.noreply.github.com>
2021-04-30 16:18:17 +0200

    Merge branch 'master' of https://github.com/Leonie-D/springProject

--- ItemProject/ExercicesSpring2021-main/src/main/java/org/lfc/controller/ViewController.java
+++ ItemProject/ExercicesSpring2021-main/src/main/java/org/lfc/controller/ViewController.java
@@ -1,16 +1,21 @@
 package org.lfc.controller;
 
+import org.lfc.entity.Item;
+import org.lfc.exception.APIException;
 import org.lfc.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/view")
+@RequestMapping("/itemview")
 public class ViewController {
 	@Autowired
 	private ItemService itemService ;
@@ -21,4 +26,22 @@ public class ViewController {
 		model.addAttribute("items", itemService.getAll());
 		return "view";
 	}
+	
+	 @PostMapping("/additem")
+	    public String create(@ModelAttribute("item") Item item) throws APIException  {
+	            itemService.create(item);
+	            return "redirect:/itemview";
+	    }
+
+
+	    @DeleteMapping("/deleteitem/{id}")
+	    public String delete(@PathVariable("id") String id) throws APIException {
+
+	            itemService.delete(id);
+	       
+
+	        return "redirect:/itemview";
+	    }
+	
+
 }
